8f1797e1e95907770b68a2b2d4eca9ed4ce2b644
wusiyu <stephenwu2019@163.com>
2019-08-24 15:24:16 +0800

    migrate security

--- 02-truffle/migrations/1_initial_migration.js
+++ 02-truffle/migrations/1_initial_migration.js
@@ -1,8 +1,9 @@
 const Migrations = artifacts.require("Migrations");
+const security = require('../../config/security')
 
 module.exports = function (deployer, network, accounts) {
   if(network == 'development')[
-    web3.eth.personal.unlockAccount(accounts[0], '123456')
+    web3.eth.personal.unlockAccount(accounts[0], security.password)
   ]
   deployer.deploy(Migrations);
 };
