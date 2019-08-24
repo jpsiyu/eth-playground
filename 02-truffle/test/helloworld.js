d4a9ef799ffbc38294fa338c35cbfb6e4daa3874
wusiyu <stephenwu2019@163.com>
2019-08-24 16:40:24 +0800

    add box

---
+++ 02-truffle/test/helloworld.js
@@ -0,0 +1,15 @@
+/*
+const HelloWorld = artifacts.require("HelloWorld")
+
+contract("HelloWorld", accounts => {
+  it("should return number 8", () => {
+    HelloWorld.deployed()
+      .then(instance => {
+        return instance.getNum()
+      })
+      .then(num => {
+        assert.equal(8, 8)
+      })
+  })
+})
+*/
