bd7ebf21897e828ad6a06e1f2b73129637d832b3
wusiyu <stephenwu2019@163.com>
2019-08-28 18:46:11 +0800

    handle big number

--- 05-bbb/test/bbb.js
+++ 05-bbb/test/bbb.js
@@ -1,4 +1,5 @@
 const BBB = artifacts.require("TokenBeiBaoBi")
+const BigNumber = require('bignumber.js')
 
 contract("BBB", accounts => {
   const owner = accounts[0]
@@ -6,22 +7,31 @@ contract("BBB", accounts => {
   const someone = accounts[2]
 
   it("first account should be owner", () => {
-    return BBB.deployed(owner. admin)
+    return BBB.deployed(owner.admin)
       .then(instance => {
         assert(instance.owner, owner)
       })
   })
 
-  it("balanceOf owner account should not be 0", () => {
+  it("balanceOf owner account should not be 47500000e18", () => {
     return BBB.deployed(owner, admin)
       .then(instance => {
         return instance.balanceOf.call(owner)
       })
       .then(balance => {
-        assert.notEqual(balance.valueOf(), 0)
+        assert.equal(balance.valueOf(), 47500000e18)
       })
   })
 
+  it("balanceOf admin account should not be 2500000e18", () => {
+    return BBB.deployed(owner, admin)
+      .then(instance => {
+        return instance.balanceOf.call(admin)
+      })
+      .then(balance => {
+        assert.equal(balance.valueOf(), 2500000e18)
+      })
+  })
   it("balanceOf random account should not be 0", () => {
     return BBB.deployed(owner, admin)
       .then(instance => {
