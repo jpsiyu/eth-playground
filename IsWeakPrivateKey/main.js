1f574e232d80563ae0d88aed2d3720e8c6e16c4a
wusiyu <stephenwu2019@163.com>
2019-09-03 19:29:54 +0800

    weak private key

---
+++ IsWeakPrivateKey/main.js
@@ -0,0 +1,35 @@
+const isWeakPrivateKey = (privKey) => {
+  let res = false
+  for (let i = 0; i < privKey.length; i++) {
+    const start = privKey[i]
+    let neighbor
+    i + 1 < privKey.length ? neighbor = privKey[i + 1] : false
+    if ((start === '0') || (neighbor === '0') || (start === neighbor)) {
+      res = true
+      break
+    }
+  }
+
+  return res
+}
+
+const test = () => {
+  const keys = [
+    '1234',
+    '0123',
+    '1023',
+    '1203',
+    '1230',
+    '1123',
+    '1223',
+    '1233',
+  ]
+  console.log('\n')
+  keys.forEach(e => {
+    const res = isWeakPrivateKey(e)
+    console.log(`is ${e} weak private key? ${res}`)
+  })
+  console.log('\n')
+}
+
+test()
