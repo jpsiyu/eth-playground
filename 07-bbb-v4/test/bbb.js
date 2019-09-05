e696cd0d81180471b050e780214431e74bee1f10
wusiyu <stephenwu2019@163.com>
2019-09-05 15:02:25 +0800

    test

--- 07-bbb-v4/test/bbb.js
+++ 07-bbb-v4/test/bbb.js
@@ -2,60 +2,70 @@ const BN = require('bignumber.js')
 const BBB = artifacts.require("TokenBeiBaoBi")
 
 contract('BBB', accounts => {
-  it("Every account should has balance 0 after deployed!", () => {
-    return BBB.deployed()
-      .then(instance => {
-        const r = Math.floor(Math.random() * accounts.length)
-        return instance.balanceOf.call(accounts[r])
-      })
-      .then(balance => {
-        assert.equal(balance.valueOf(), 0)
-      })
-  })
-
-})
-
-contract('BBB loadUserAmount', accounts => {
   const owner = accounts[0]
   const admin = accounts[1]
   const someoneA = accounts[2]
   const someoneB = accounts[3]
   const someoneC = accounts[4]
 
-  it('Only owner can call loadUserAmount', () => {
-    let instance
-    return BBB.deployed()
-      .then(_instance => {
-        instance = _instance
-        return instance.loadUserAmount(someoneA, 100)
-      })
-      .then(_ => {
-        return instance.balanceOf(someoneA)
-      })
-      .then(balance => {
-        assert.equal(balance, 100)
-      })
+  describe('initialize', () => {
+    let ins
+    before(() => {
+      return BBB.deployed()
+        .then(_ins => {
+          ins = _ins
+        })
+    })
+    it("Every account should has balance 0 after deployed!", () => {
+      const r = Math.floor(Math.random() * accounts.length)
+      return ins.balanceOf(accounts[r])
+        .then(balance => {
+          assert.equal(balance.valueOf(), 0)
+        })
+    })
   })
 
-  it('load multi user amount', () => {
-    let instance
-    return BBB.deployed()
-      .then(_instance => {
-        instance = _instance
-      })
-      .then(() => {
-        return instance.loadOldTokenUserAmount([someoneB, someoneC], [10, 20])
-      })
-      .then((res) => {
-        return Promise.all([
-          instance.balanceOf.call(someoneB),
-          instance.balanceOf.call(someoneC),
-        ])
-      })
-      .then(res => {
-        const [balanceOfB, balanceOfC] = res
-        assert.equal(balanceOfB, 10)
-        assert.equal(balanceOfC, 20)
-      })
+  describe('loadUserAmount', () => {
+    let ins
+    before(() => {
+      return BBB.deployed()
+        .then(_ins => {
+          ins = _ins
+        })
+    })
+    it('Only owner can call loadUserAmount', () => {
+      const amount = 100
+      return ins.loadUserAmount(someoneA, amount)
+        .then(() => {
+          return ins.balanceOf(someoneA)
+        })
+        .then(balance => {
+          assert.equal(balance, amount)
+        })
+    })
+    it('load multi user amount', () => {
+      let instance
+      let amountB = 10
+      let amountC = 20
+      return BBB.deployed()
+        .then(_instance => {
+          instance = _instance
+        })
+        .then(() => {
+          return instance.loadOldTokenUserAmount([someoneB, someoneC], [amountB, amountC])
+        })
+        .then((res) => {
+          return Promise.all([
+            instance.balanceOf.call(someoneB),
+            instance.balanceOf.call(someoneC),
+          ])
+        })
+        .then(res => {
+          const [balanceOfB, balanceOfC] = res
+          //console.log('balance:', BN(balanceOfB).toNumber())
+          assert.equal(balanceOfB, amountB)
+          assert.equal(balanceOfC, amountC)
+        })
+    })
   })
 })
