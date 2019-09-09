d09ea1322a5fbc4f53dcd23b09d1aa66ae4b0001
wusiyu <stephenwu2019@163.com>
2019-09-09 21:09:35 +0800

    how hd wallet work

--- ethersjs/demo/hd_node.js
+++ ethersjs/demo/hd_node.js
@@ -5,9 +5,28 @@ const consola = require('consola')
 const main = () => {
   const mnemonic = 'radar blur cabbage chef fix engine embark joy scheme fiction master release'
   const masterNode = HDNode.fromMnemonic(mnemonic, ethers.wordlists.en, 'heloji')
-  const ethNode = masterNode.derivePath("m/44'/60'/0'/0/0")
-  const xpriv = ethNode.extendedKey
-  const xpub = ethNode.neuter().extendedKey
-  consola.info(`master node info, priv: ${xpriv}, pub: ${xpub}`)
+  consola.info('master node')
+  console.table(masterNode)
+
+  const masterEthNode = masterNode.derivePath("m/44'/60'/0'/0")
+  console.table(masterEthNode)
+
+  const p0 = masterEthNode.derivePath('0')
+  consola.info('node 0')
+  console.table(p0)
+
+  const p1 = masterEthNode.derivePath('1')
+  consola.info('node 1')
+  console.table(p1)
+
+  const p01 = masterEthNode.derivePath('0/1')
+  consola.info('node 0/1')
+  console.table(p01)
+
+  const p11 = masterEthNode.derivePath('1/1')
+  consola.info('node 1/1')
+  console.table(p11)
+
+
 }
 main()
