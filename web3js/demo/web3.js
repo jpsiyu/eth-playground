85144c8b4c26f58361097c5f9d88323cdbb73a3b
wusiyu <stephenwu2019@163.com>
2019-09-10 14:51:54 +0800

    show info

--- web3js/demo/web3.js
+++ web3js/demo/web3.js
@@ -3,10 +3,15 @@ const consola = require('consola')
 
 const main = () => {
   consola.info('version', Web3.version)
-  consola.info('providers', Web3.providers)
-  consola.info('modules', Object.keys(Web3.modules))
 
-  const url = 'http://localhost:9545'
+  consola.info('providers')
+  console.table(Web3.providers)
+
+  consola.info('modules')
+  console.table(Web3.modules)
+
+  //const url = 'http://localhost:9545'
+  const url = 'ws://localhost:9545'
   const web3 = new Web3(url)
   web3.eth.net.getId()
     .then(id => consola.info('networkd id', id))
