15be28ed7ada42d16695375017ffc10f02f49fda
wusiyu <stephenwu2019@163.com>
2019-12-13 18:00:30 +0800

    send message

--- 16-whipser/main.go
+++ 16-whipser/main.go
@@ -3,17 +3,44 @@ package main
 import (
 	"fmt"
 	"log"
+	"context"
 
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/whisper/shhclient"
+	"github.com/ethereum/go-ethereum/whisper/whisperv6"
 )
 
 func main() {
 	fmt.Println("haha")
-	//client, err := ethclient.Dial("http://localhost:8545")
-	client, err := ethclient.Dial("ws://192.168.0.111:8546")
+	client, err := shhclient.Dial("ws://127.0.0.1:8546")
+	//client, err := shhclient.Dial("ws://192.168.0.111:8546")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("connection established")
-	_ = client
+
+	keyID, err := client.NewKeyPair(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("key id", keyID)
+
+	pub, err := client.PublicKey(context.Background(), keyID)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("pub key", keyID, hexutil.Encode(pub))
+
+	message := whisperv6.NewMessage{
+		Payload: []byte("hello"),
+		PublicKey: pub,
+		TTL: 60,
+		PowTime: 2,
+		PowTarget: 2.5,
+	}
+
+	hash, err := client.Post(context.Background(), message)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("hash", hash)
 }
