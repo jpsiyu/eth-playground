158c234c58291a9bbce93b65804be1a28377e624
wusiyu <stephenwu2019@163.com>
2019-12-14 10:44:21 +0800

    subscribe

--- 16-whipser/main.go
+++ 16-whipser/main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"log"
 	"context"
+	"os"
+	"runtime"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/whisper/shhclient"
 	"github.com/ethereum/go-ethereum/whisper/whisperv6"
 )
 
 func main() {
-	fmt.Println("haha")
 	client, err := shhclient.Dial("ws://127.0.0.1:8546")
 	//client, err := shhclient.Dial("ws://192.168.0.111:8546")
 	if err != nil {
@@ -22,13 +22,34 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("key id", keyID)
 
+	// subscribe message
+	messages := make(chan *whisperv6.Message)
+	criteria := whisperv6.Criteria{
+		PrivateKeyID: keyID,
+	}
+	sub, err := client.SubscribeMessages(context.Background(), criteria, messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func(){
+		for {
+			select{
+			case err := <-sub.Err():
+				log.Fatal(err)
+			case message := <- messages:
+				fmt.Println(string(message.Payload))
+				os.Exit(0)
+			}
+		}
+	}()
+
+	// send message
 	pub, err := client.PublicKey(context.Background(), keyID)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("pub key", keyID, hexutil.Encode(pub))
 
 	message := whisperv6.NewMessage{
 		Payload: []byte("hello"),
@@ -39,8 +60,10 @@ func main() {
 	}
 
 	hash, err := client.Post(context.Background(), message)
+	_ = hash
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("hash", hash)
+
+	runtime.Goexit()
 }
